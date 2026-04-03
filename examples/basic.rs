c9b1cfe78e9f2222476771779cd9a375bb796521
MK <mahadk20@gmail.com>
2026-04-03 17:26:33 +0500

    fix: update to match new api.

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,8 +1,53 @@
 use std::sync::Arc;
 
-use workerv2::{Handler, Worker};
+use async_trait::async_trait;
+use serde::{Deserialize, Serialize};
+
+use workerv2::{HandleOutcome, HandlesJob, Worker};
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+#[serde(tag = "type")]
+pub enum JobPayload {
+    Generate(GeneratePayload),
+    Resolve(ResolvePayload),
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub struct GeneratePayload {
+    pub id: i64,
+    pub min: i64,
+    pub max: i64,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub struct ResolvePayload {
+    pub id: i64,
+    pub sleep_ms: u64,
+}
 
 const DEFAULT_NUM_WORKERS: usize = 1;
+
+pub struct Handler {}
+
+#[async_trait]
+impl HandlesJob<JobPayload> for Handler {
+    async fn handle_job(&self, job_payload: &JobPayload) -> anyhow::Result<HandleOutcome> {
+        match job_payload {
+            JobPayload::Generate(_payload) => {
+                println!("Generate payload.");
+                tokio::time::sleep(tokio::time::Duration::from_millis(0)).await;
+                Ok(HandleOutcome::Succeeded)
+            }
+            JobPayload::Resolve(_payload) => {
+                println!("Resolve payload.");
+                tokio::time::sleep(tokio::time::Duration::from_millis(0)).await;
+                Ok(HandleOutcome::Retry {
+                    reason: String::from("just testing"),
+                })
+            }
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -19,6 +64,8 @@ async fn main() -> anyhow::Result<()> {
     }
 
     let worker = Worker::new(num_workers, Arc::new(Handler {})).await?;
+
+    // inserting some tasks
 
     worker.start().await?;
 
